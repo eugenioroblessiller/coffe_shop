9382250a6d5efc8fad92a25d7b788c3c41532fa0
eugenio <eugenio@baliotech.com>
2022-03-10 20:11:34 -0600

    added foreing key

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -99,8 +99,10 @@ class Show(db.Model):
     __tablename__ = 'shows'
 
     id = db.Column(db.Integer, primary_key=True)
-    artist_id = db.Column(db.Integer, nullable=False)
-    venue_id = db.Column(db.Integer, nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey(
+        'artists.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey(
+        'venues.id'), nullable=False)
     start_time = db.Column(db.DateTime(), nullable=False)
 
     venue = db.relationship('Venue')
