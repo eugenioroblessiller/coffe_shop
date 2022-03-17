5836b49f46e1c8639ebabf96c9d1c789838c25fa
eugenio <eugenio@baliotech.com>
2022-03-16 19:57:25 -0600

    update artist working

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -2,6 +2,7 @@
 # Imports
 #----------------------------------------------------------------------------#
 
+from unicodedata import name
 from forms import *
 from flask_wtf import Form
 from logging import Formatter, FileHandler
@@ -231,8 +232,6 @@ def show_venue(venue_id):
     data['past_shows_count'] = len(past_shows)
     data['upcoming_shows_count'] = len(upcoming_shows)
 
-    print('Venue---------------------------')
-    print(data)
     # data1 = {
     #     "id": 1,
     #     "name": "The Musical Hop",
@@ -514,20 +513,33 @@ def show_artist(artist_id):
 
 @app.route('/artists/<int:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id):
-    form = ArtistForm()
-    artist = {
-        "id": 4,
-        "name": "Guns N Petals",
-        "genres": ["Rock n Roll"],
-        "city": "San Francisco",
-        "state": "CA",
-        "phone": "326-123-5000",
-        "website": "https://www.gunsnpetalsband.com",
-        "facebook_link": "https://www.facebook.com/GunsNPetals",
-        "seeking_venue": True,
-        "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-        "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
-    }
+    artist = Artist.query.filter_by(id=artist_id).first_or_404()
+    form = ArtistForm(
+        name= artist.name,
+        genres= artist.genres,
+        city= artist.city,
+        state= artist.state,
+        phone= artist.phone,
+        website_link=artist.website_link,
+        facebook_link= artist.facebook_link,
+        seeking_venue= artist.seeking_venue,
+        seeking_description= artist.seeking_description,
+        image_link= artist.image_link
+    )
+
+    # artist = {
+    #     "id": obj.id,
+    #     "name": obj.name,
+    #     "genres": obj.genres,
+    #     "city": obj.city,
+    #     "state": obj.state,
+    #     "phone": obj.phone,
+    #     "website": obj.website_link,
+    #     "facebook_link": obj.facebook_link,
+    #     "seeking_venue": obj.seeking_venue,
+    #     "seeking_description": obj.seeking_description,
+    #     "image_link": obj.image_link
+    # }
     # TODO: populate form with fields from artist with ID <artist_id>
     return render_template('forms/edit_artist.html', form=form, artist=artist)
 
@@ -536,7 +548,33 @@ def edit_artist(artist_id):
 def edit_artist_submission(artist_id):
     # TODO: take values from the form submitted, and update existing
     # artist record with ID <artist_id> using the new attributes
-
+    error = False
+    try:
+        artist = Artist.query.filter_by(id=artist_id).first_or_404()
+        artist.name = request.form['name']
+        artist.city = request.form['city']
+        artist.state = request.form['state']
+        artist.phone = request.form['phone']
+        artist.genres = request.form.getlist('genres')
+        artist.facebook_link = request.form['facebook_link']
+        artist.image_link = request.form['image_link']
+        artist.website_link = request.form['website_link']
+        artist.seeking_venue = 'seeking_venue' in request.form
+        artist.seeking_description = request.form['seeking_description']
+        db.session.commit()
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+        if error:
+            flash('An error occurred. Artist ' +
+                  request.form['name'] + ' could not be updated.')
+        else:
+            flash('Artist ' + request.form['name'] +
+                  ' was successfully updated!')
+        
     return redirect(url_for('show_artist', artist_id=artist_id))
 
 
